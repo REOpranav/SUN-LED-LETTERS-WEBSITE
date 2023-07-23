0a87ef02a98e4641f8f029599fa88c66e55837b2
REOpranav <pranavavadivel01@gmail.com>
2023-07-23 22:38:05 +0530

    Adding new components

---
+++ src/TopList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TopList = () => {
+  return (
+    <div className='topList'>
+   
+        <ul className='ul-list'>
+            <li id='features'>features</li>
+            <li id='company'>company</li>
+            <li id='support'>support</li>
+            <li id='pricing'>pricing </li>
+            <li id='contact'>contact</li>
+        </ul>
+   
+    </div>
+  )
+}
+
+export default TopList
