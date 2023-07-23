896dba1673e7cdd03b49f2ebcc2a93cef7129145
REOpranav <pranavavadivel01@gmail.com>
2023-07-23 22:37:15 +0530

    Adding new components

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+<div className='header'>   
+   <div class="chain-container" >
+     
+        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px blue'}}>        <div class="chain-box">s </div> </p>
+        <p className='sign-board' style={{boxShadow:'inset 1px -1px 10px red'}}>        <div class="chain-box">u </div> </p>
+        <p className='sign-board' style={{boxShadow:'inset -1px -1px 10px green'}}>     <div class="chain-box">n </div></p> 
+       
+        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px blue'}}>        <div class="chain-box">l</div></p>
+        <p className='sign-board' style={{boxShadow:'inset 1px -1px 10px red'}}>        <div class="chain-box">e</div></p>
+        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px green'}}>       <div class="chain-box">d</div></p>
+   
+        <p className='sign-board' style={{boxShadow:'inset -1px 1px 10px white'}}>      <div class="chain-box">l</div></p>
+        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px  green'}}>      <div class="chain-box">e </div></p>
+        <p className='sign-board' style={{boxShadow:'inset -1px -1px 10px darkblue'}}>  <div class="chain-box">t </div></p>
+        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px skyblue '}}>    <div class="chain-box">t </div></p>
+        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px  brown'}}>      <div class="chain-box">e </div></p>
+        <p className='sign-board' style={{boxShadow:'inset -1px 1px 10px red'}}>        <div class="chain-box">r </div></p>
+        <p className='sign-board' style={{boxShadow:'inset 1px -1px 10px blue'}}>        <div class="chain-box">s </div></p>
+  
+  </div>
+</div>
+  )
+}
+
+export default Header
