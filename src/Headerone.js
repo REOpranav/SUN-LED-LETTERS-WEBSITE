33c1153b4af6fb4d037281015bf5fad20b9320e3
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 23:01:41 +0530

    Add headerone compo

---
+++ src/Headerone.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Headerone = () => {
+  return (
+    
+         <header>
+            sing board design
+         </header>
+  
+  )
+}
+
+export default Headerone
