f6722aac682001b32ed7a74e466baa75793b2484
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 22:59:36 +0530

    Add email components

---
+++ src/Email.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Email = () => {
+  return (
+    <div className='email'>
+         <a href="mailto: pranavavadivel01@gmail.com">pranavavadivel01@gmail.com</a>
+    </div>
+  )
+}
+
+export default Email
