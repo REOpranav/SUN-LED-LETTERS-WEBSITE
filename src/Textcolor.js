31e0f236150bc10c85ce2e47d0f1902f5b950fd7
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 23:06:27 +0530

    Add text color components

---
+++ src/Textcolor.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Textcolor = ({textcolor,settextcolor}) => {
+  return (
+    <div>
+         
+             <select 
+                  className='optionTag'
+                  value={textcolor}
+                  onChange={(e)=>{settextcolor(e.target.value)}}
+             >      
+                    <option>select text color</option>
+                    <option value="white">white</option>
+                    <option value="darkRed">darkRed</option>
+                    <option value="Green">Green</option>
+                    <option value="Red">Red</option>
+                    <option value="Orange">Orange</option>
+                    <option value="Yellow">Yellow</option>
+                    <option value="Lime">Lime</option>
+                    <option value="Blue">Blue</option>
+                    <option value="Magenta">Magenta</option>
+                    <option value="Cyan">Cyan</option>
+                    <option value="Pink">Pink</option>
+                    <option value="Purple">Purple</option>
+                    <option value="Teal">Teal</option>
+                    <option value="orangered">Orange-Red</option>
+                    <option value="Green">Green</option>
+                    <option value = "black">black</option>
+                    
+
+             </select>
+        
+    </div>
+  )
+}
+
+export default Textcolor
