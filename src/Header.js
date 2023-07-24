f1587d80009f52dfe0941ada471c088a1acb5e53
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 23:01:19 +0530

    Changing backgrounds

--- src/Header.js
+++ src/Header.js
@@ -4,22 +4,22 @@ const Header = () => {
   return (
 <div className='header'>   
    <div class="chain-container" >
-     
-        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px blue'}}>        <div class="chain-box">s </div> </p>
-        <p className='sign-board' style={{boxShadow:'inset 1px -1px 10px red'}}>        <div class="chain-box">u </div> </p>
-        <p className='sign-board' style={{boxShadow:'inset -1px -1px 10px green'}}>     <div class="chain-box">n </div></p> 
        
-        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px blue'}}>        <div class="chain-box">l</div></p>
-        <p className='sign-board' style={{boxShadow:'inset 1px -1px 10px red'}}>        <div class="chain-box">e</div></p>
-        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px green'}}>       <div class="chain-box">d</div></p>
-   
-        <p className='sign-board' style={{boxShadow:'inset -1px 1px 10px white'}}>      <div class="chain-box">l</div></p>
-        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px  green'}}>      <div class="chain-box">e </div></p>
-        <p className='sign-board' style={{boxShadow:'inset -1px -1px 10px darkblue'}}>  <div class="chain-box">t </div></p>
-        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px skyblue '}}>    <div class="chain-box">t </div></p>
-        <p className='sign-board' style={{boxShadow:'inset 1px 1px 10px  brown'}}>      <div class="chain-box">e </div></p>
-        <p className='sign-board' style={{boxShadow:'inset -1px 1px 10px red'}}>        <div class="chain-box">r </div></p>
-        <p className='sign-board' style={{boxShadow:'inset 1px -1px 10px blue'}}>        <div class="chain-box">s </div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px blue',backgroundColor: 'blue'}}>      <div class="chain-box">s </div> </p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px red',backgroundColor: 'red'}}>        <div class="chain-box">u </div> </p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px green',backgroundColor: 'green'}}>     <div class="chain-box">n </div></p> 
+        <p style={{color : 'yellow'}}>. .</p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px gray',backgroundColor: 'gray'}}>      <div class="chain-box">l</div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px orange',backgroundColor: 'orange'}}>       <div class="chain-box">e</div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px pink',backgroundColor: 'pink'}}>     <div class="chain-box">d</div></p>
+        <p style={{color : 'yellow'}}>. .</p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px #333',backgroundColor: '#333'}}>     <div class="chain-box">l</div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px lime',backgroundColor: 'lime'}}>     <div class="chain-box">e </div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px darkblue',backgroundColor: 'darkblue'}}>  <div class="chain-box">t </div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px brown',backgroundColor: 'brown '}}>  <div class="chain-box">t </div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px violet',backgroundColor: 'violet'}}>     <div class="chain-box">e </div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px orangered',backgroundColor: 'orangered'}}>       <div class="chain-box">r </div></p>
+        <p className='sign-board' style={{boxShadow: '5px 5px 40px gold',backgroundColor: 'gold'}}>      <div class="chain-box">s </div></p>
   
   </div>
 </div>
