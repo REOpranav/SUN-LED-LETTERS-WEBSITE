3e56895c82d3eb5d8036ff219799c0859f5706b0
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 23:08:06 +0530

    add text weight components

---
+++ src/Textweight.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Textweight = ({textweight,settextweight}) => {
+  return (
+    <div>
+        <select
+                className='optionTag'     
+                value={textweight}
+                onChange={(e)=>settextweight(e.target.value)}
+         >
+             <option>Select Font Weight</option>
+             <option value="bold">Bold</option>
+             <option value="bolder">Bolder</option>
+             <option value="lighter">Lighter</option>
+             <option value="100">100</option>
+             <option value="200">200</option>
+             <option value="300">300</option>
+             <option value="400">400</option>
+             <option value="500">500</option>
+             <option value="600">600</option>
+             
+        </select>
+    </div>
+  )
+}
+
+export default Textweight
