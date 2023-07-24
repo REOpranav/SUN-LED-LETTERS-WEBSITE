5d807588419296017d99fa5dc600a78d69418ea0
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 23:09:17 +0530

    add top list components

--- src/TopList.js
+++ src/TopList.js
@@ -3,16 +3,19 @@ import React from 'react'
 const TopList = () => {
   return (
     <div className='topList'>
-   
+     <div>
         <ul className='ul-list'>
-            <li id='features'>features</li>
-            <li id='company'>company</li>
-            <li id='support'>support</li>
-            <li id='pricing'>pricing </li>
-            <li id='contact'>contact</li>
+
+          <a href="#features"> <li>features</li></a>
+          <a href="#direction"> <li>company</li></a>
+          <a href="#design"> <li>design</li></a>
+          <a href="#prize"> <li>prize</li></a> 
+          <a href="#contact"> <li>contact</li></a>
+
         </ul>
-   
+      
     </div>
+   </div>
   )
 }
 
