914c593fcf93fd0e14828a11fae084f263691c30
REOpranav <pranavavadivel01@gmail.com>
2023-07-24 23:04:12 +0530

    Add quote components

---
+++ src/Quotes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Quotes = () => {
+  return (
+    <div className='quotes'>
+      <div className='content'>
+            <p> the art of hiring </p>
+            <p className='para-q'>Do you need an artist to breathe art in you company ? Here's the super easy platform to the perfect illustrator ,2D and 3D artist. Connect and make it beautfull </p>
+      </div>
+     
+      <img src="https://media.istockphoto.com/id/1260517250/vector/black-light-bulb-icon-isolated-on-yellow-background-energy-and-idea-symbol-lamp-electric.jpg?s=170667a&w=0&k=20&c=VJuwEcLNuVlkGFmwpjWD_AioB4PSeguMwFcplAYvRn8=" 
+            className='image'
+        />
+
+    </div>
+  )
+}
+
+export default Quotes
